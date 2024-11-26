773504060e5f59190a1eab29106adbd558971d62
Awsawoad <iyasawoad86@gmail.com>
2024-11-26 04:01:38 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Accordion from './componant/Accordion/Accordion';
+
+const App = () => {
+    return (
+        <div style={{ padding: '20px' }}>
+<p>Get the best villa agency HTML CSS Bootstrap Template for your company website.
+ TemplateMo provides you the best free CSS templates in the world. 
+ Please tell your friends about it. Thank you.
+Cloud bread kogi bitters pitchfork shoreditch tumblr yr succulents single-origin coffee schlitz
+ enamel pin you probably havent heard of them ugh hella.
+</p>
+ 
+
+<p>
+When you look for free CSS templates, you can simply type TemplateMo in any search engine website. In addition, 
+you can type TemplateMo Digital Marketing, TemplateMo Corporate Layouts, etc.
+ Master cleanse +1 intelligentsia swag post-ironic,
+  slow-carb chambray knausgaard PBR&B DSA poutine neutra cardigan hoodie pop-up.
+  </p>
+           
+            <Accordion />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/componant/Accordion/Accordion.jsx
@@ -0,0 +1,40 @@
+
+
+export default function Accordion() {
+    return (
+        <div className="accordion" id="accordionExample">
+            <div className="accordion-item">
+                <h2 className="accordion-header">
+                    <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Best useful links ?
+                    </button>
+                </h2>
+                <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                    <div className="accordion-body">Get <strong>the best villa</strong> website template in HTML CSS and Bootstrap for your business. TemplateMo provides you the <a href="https://www.google.com/search?q=best+free+css+templates" target="_blank">best free CSS templates</a> in the world. Please tell your friends about it.</div>
+                </div>
+            </div>
+
+
+            <div className="accordion-item">
+                <h2 className="accordion-header">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        How does this work ?
+                    </button>
+                </h2>
+                <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div className="accordion-body">Dolor <strong>almesit amet</strong>, consectetur adipiscing elit, sed doesnt eiusmod tempor incididunt ut labore consectetur <code>adipiscing</code> elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+                </div>
+            </div>
+            <div className="accordion-item">
+                <h2 className="accordion-header">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        Why is Villa Agency the best ?
+                    </button>
+                </h2>
+                <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div className="accordion-body">Dolor <strong>almesit amet</strong>, consectetur adipiscing elit, sed doesnt eiusmod tempor incididunt ut labore consectetur <code>adipiscing</code> elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+                </div>
+            </div>
+        </div>
+    )
+}
